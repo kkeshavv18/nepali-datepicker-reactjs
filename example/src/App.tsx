e28e69747ce2a4c51092c70203822a44f0c095be
kkeshavv18 <kkeshavv18@gmail.com>
2025-06-13 19:03:08 +0545

    feat: create an example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,11 +5,20 @@ import "@kkeshavv18/nepali-datepicker/dist/index.css";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState("2081-01-15");
+  const handleDateChange = (date: string) => {
+    console.log("selected Date:", date);
+    setSelectedDate(date);
+  };
   return (
     <>
       <NepaliDatePicker
         initialDate={selectedDate}
-        onDateChange={(date: string) => setSelectedDate(date)}
+        onDateChange={handleDateChange}
+        width="300px"
+        helperText="Select a date"
+        helperTextColor="#6b7280"
+        helperTextFontSize="15px"
+        closeOnSelect={false}
       />
     </>
   );
